d118a5ebcd8cc64c4f22d76243a793a651b973d6
glw <510537547@qq.com>
2019-06-27 15:12:55 +0800

    【添加】添加启动类和相关配置

---
+++ manager/src/main/java/com/glw/manager/ManagerApplication.java
@@ -0,0 +1,17 @@
+package com.glw.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : glw
+ * @date : 2019/6/27
+ * @time : 11:31
+ * @Description: 产品启动类
+ */
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class);
+    }
+}
